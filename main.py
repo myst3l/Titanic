8b6df2b3e82815153552c46a47c3cb70b1bcfb26
crispymayo <karakumayl@gmail.com>
2023-03-04 20:17:21 -0500

    from pc

--- main.py
+++ main.py
@@ -1,24 +1,57 @@
 import pandas as pd
+import re
+
+# pd.set_option('display.height', 500)
+pd.set_option('display.max_rows', None)
+
+df = pd.read_csv('train.csv')
+pd.set_option('display.width', 150)
+pd.set_option('display.max_columns', None)
 
 
+def p():
+    print(df)
 
-holder = pd.read_csv('train.csv')
 
-df = holder[["PassengerId", "Survived", "Pclass", "Sex", "Fare"]]
-df.loc[df['Sex'] == 'female', 'Sex'] = 2
+def sex_check():
+    df.loc[df['Sex'] == 1,  'ratio'] = df.ratio*0.8
+    df.loc[df['Sex'] == 2,  'ratio'] = df.ratio*1.2
+def age
+
+# df = holder[["PassengerId", "Survived", "Age", "Pclass", "Sex", "Fare"]]
+# df.loc[df['Sex'] == 'female', 'Sex'] = "1"
+# df.loc[df['Sex'] == 'male', 'Sex'] = "0"
+# df.Sex.replace(['male', 'female'], [0, 1], inplace=True)
+
+# srv = df.loc[df['Survived'] == 1]
+# ded = df.loc[df['Survived'] == 0]
+# srv.reset_index(drop=True, inplace=True)
+# ded.reset_index(drop=True, inplace=True)
+
+# print(df)
+# df.to_csv('checkpoint.csv')
+# print(df.dtypes)
+# print(srv.drop('PassengerId', axis=1).mean())
+# print(df.drop(['PassengerId', 'Survived'], axis=1).describe())
+# print(df.drop('PassengerId', axis=1).groupby(['Survived']).mean())
+#
+# print(srv.drop(['PassengerId', 'Survived'], axis=1).describe())
+# print(ded.drop(['PassengerId', 'Survived'], axis=1).describe())
+
+# STARTING FRESH
+
+
+df.drop(columns=['Name', 'Ticket'], inplace=True)
+
+df['ratio'] = 1
+
 df.loc[df['Sex'] == 'male', 'Sex'] = 1
+df.loc[df['Sex'] == 'female', 'Sex'] = 2
 
-srv = df.loc[df['Survived'] == 1]
-ded = df.loc[df['Survived'] == 0]
-srv.reset_index(drop=True, inplace=True)
-ded.reset_index(drop=True, inplace=True)
-print(srv)
-print(ded)
+# df['count'] = 0
+# print(df.groupby(['Survived', 'Sex', 'Pclass']).count()['count'])
 
-srv.to_csv('survivors.csv')
 
-#myst3l
 
-# love from mac
-# love from pc
-# love from browser xo
+sex_check()
+p()
